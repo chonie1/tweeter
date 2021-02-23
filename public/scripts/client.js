a95575c91aa43a833f2924ecc0922344a334e031
Connie Ho <connie3@ualberta.ca>
2021-02-23 23:16:14 +0000

    add responsive design

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,89 +4,127 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// escape function to prevent XSS
+const escape =  function(str) {
+  let div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
+
+// creating a single tweet element
+const createTweetElement = function(tweetData) {
+  const daysAgo = Math.floor((Date.now() - tweetData.created_at) / (3600 * 24));
+  let dayString = null;
+
+  if (daysAgo >= 365) {
+    const years = Math.floor(daysAgo / 365);
+    dayString = `${years} years and ${Math.floor(daysAgo % 365)} days ago`;
+  } else if (daysAgo === 1) {
+    dayString = `${daysAgo} day ago`;
+  } else {
+    dayString = `${daysAgo} days ago`;
+  }
+
+  const $tweet =
+  `<article>
+    <header>
+      <div class="user">
+        <img src="${tweetData.user.avatars}">
+        <span class="tweet-name">${tweetData.user.name}</span>
+      </div>
+      <div class="username">${tweetData.user.handle}</div>
+    </header>
+    <main>
+      <p>${escape(tweetData.content.text)}</p>
+      <hr>
+    </main>
+    <footer>
+      <div>${dayString}</div>
+      <div>
+        <div class="fas fa-flag icon"></div>
+        <div class="fas fa-retweet icon"></div>
+        <div class="fas fa-heart icon"></div>
+      </div>
+    </footer>
+   </article>`;
+  
+  return $tweet;
+};
+
+// loops through tweets
+// calls createTweetElement for each tweet
+// takes return value and appends it to the tweets container
+const renderTweets = function(tweets) {
+
+  //clear any tweets first
+  $('#tweets-container').empty();
+
+  // sort by most recent
+  const sortedTweets = Object.values(tweets)
+    .sort((a, b) => (a.created_at < b.created_at) ? 1 : (a.created_at > b.created_at) ? -1 : 0);
+
+  for (const tweet of sortedTweets) {
+    $('#tweets-container').append(createTweetElement(tweet));
+  }
+  return;
+};
+
+// Fetching tweets from /tweets route
+const loadTweets = function() {
+  $.ajax({
+    method: 'GET',
+    url: '/tweets',
+    dataType: 'json'
+  })
+    .done(function(data) {
+      renderTweets(data);
+    })
+    .fail(function(msg) {
+      console.log(`This failed: ${msg}`);
+    });
+};
+
 $(document).ready(function() {
-  // creating a single tweet element
-  const createTweetElement = function(tweetData) {
-    const daysAgo = Math.floor((Date.now() - tweetData.created_at) / (3600 * 24));
-    let dayString = null;
-
-    if (daysAgo >= 365) {
-      const years = Math.floor(daysAgo / 365);
-      dayString = `${years} years and ${Math.floor(daysAgo % 365)} days ago`;
-    } else if (daysAgo === 1) {
-      dayString = `${daysAgo} day ago`;
-    } else {
-      dayString = `${daysAgo} days ago`;
-    }
-
-    const $tweet =
-    `<article>
-      <header>
-        <div class="user">
-          <img src="${tweetData.user.avatars}">
-          <span class="tweet-name">${tweetData.user.name}</span>
-        </div>
-        <div class="username">${tweetData.user.handle}</div>
-      </header>
-      <main>
-        <p>${tweetData.content.text}</p>
-        <hr>
-      </main>
-      <footer>
-        <div>${dayString}</div>
-        <div>
-          <div class="fas fa-flag icon"></div>
-          <div class="fas fa-retweet icon"></div>
-          <div class="fas fa-heart icon"></div>
-        </div>
-      </footer>
-     </article>`;
-    
-    return $tweet;
-  };
-
-  // loops through tweets
-  // calls createTweetElement for each tweet
-  // takes return value and appends it to the tweets container
-  const renderTweets = function(tweets) {
-
-    for (const tweet of Object.values(tweets)) {
-      $('#tweets-container').append(createTweetElement(tweet));
-    }
-    return;
-  };
+  
+  // hiding error message on load
+  $('#error').hide();
 
   // Ajax request form submission for new tweet
   $('#new-tweet').submit(function(e) {
     e.preventDefault();
+
+    // hiding error if there is one already
+    $('#error').slideUp();
+    
+    // validating form
+    const counter = Number($(this).find('.counter').val());
+
+    if (counter < 0) {
+      $('#error').html('<span class="fas fa-exclamation-triangle"></span><span>Chill, this ain\'t yo diary</span><span class="fas fa-exclamation-triangle"></span>');
+      $('#error').slideDown();
+    }
+
+    if (counter === 140) {
+      $('#error').html('<span class="fas fa-exclamation-triangle"></span><span>Did you even try?</span><span class="fas fa-exclamation-triangle"></span>');
+      $('#error').slideDown();
+      return;
+    }
+
     const inputs = $(this).serialize();
+
     $.ajax({
       method: 'POST',
       url: '/tweets',
       data: inputs
     })
       .done(function(msg) {
+        loadTweets();
         console.log(`Success! ${msg}`);
       })
       .fail(function(msg) {
-        console.log(`This failed: ${msg}`)
+        console.log(`This failed: ${msg}`);
       });
-
-  // Fetching tweets from /tweets route
-  const loadTweets = function() {
-    $.ajax({
-      method: 'GET',
-      url: '/tweets',
-      dataType: 'json'
-    })
-      .done(function(data) {
-        renderTweets(data);
-      })
-      .fail(function(msg) {
-        console.log(`This failed: ${msg}`)
-      });
-  };
-
-    loadTweets();
   });
+
+  loadTweets();
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,11 +3,13 @@ $(document).ready(function() {
   // Function keeps track of how many characters have been inputted in the text area
   $("#tweet-text").on('input', function() {
     let lettersLeft = 140 - $(this).val().length;
-    const counter = $(this).siblings().children()[1];
+    const counter = $(this).parent().find('.counter');
     $(counter).text(lettersLeft);
     
     if (lettersLeft < 0) {
       $(counter).css('color', 'red');
+    } else {
+      $(counter).css('color', 'black');
     }
   });
 
